9aec101c75efd47a8a577e2e316e45f9f7d9676d
paveldt <paveldt2@gmail.com>
2020-04-04 21:54:31 +0100

    Implementing simple if statement in Go

--- test.go
+++ test.go
@@ -6,5 +6,15 @@ import (
 )
 
 func main() {
-	fmt.Println("this is just a test for go")
+	fmt.Println("this is a GoLang practice")
+
+	x := 100
+
+	if x == 50 {
+		fmt.Println("Germany")
+	} else if x == 100 {
+		fmt.Println("Japan")
+	} else {
+		fmt.Println("Canada")
+	}
 }
